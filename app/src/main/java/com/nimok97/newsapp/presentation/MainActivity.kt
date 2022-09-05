a209f6544e4b4b45342d7e1fe94d94d93c2748cc
한창희 <nimok97@gmail.com>
2022-09-06 00:40:04 +0900

    [FEAT] 메인 액티비티 레이아웃 작업

--- app/src/main/java/com/nimok97/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/nimok97/newsapp/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package com.nimok97.newsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.nimok97.newsapp.R
+import com.nimok97.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
