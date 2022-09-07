ab49dab14c1cbf0f5dd193025372efb924ec9a0e
한창희 <nimok97@gmail.com>
2022-09-07 23:06:52 +0900

    [REFACTOR] repository에서 data model을 domain model로 매핑하도록 수정

--- app/src/main/java/com/nimok97/newsapp/data/remote/repository/NewsRemoteRepositoryImpl.kt
+++ app/src/main/java/com/nimok97/newsapp/data/remote/repository/NewsRemoteRepositoryImpl.kt
@@ -3,10 +3,13 @@ package com.nimok97.newsapp.data.remote.repository
 import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.map
 import com.nimok97.newsapp.data.remote.api.NewsService
 import com.nimok97.newsapp.data.remote.datasource.NewsRemotePagingSource
+import com.nimok97.newsapp.data.remote.mapper.RemoteMapper
 import com.nimok97.newsapp.domain.repository.remote.NewsRemoteRepository
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class NewsRemoteRepositoryImpl @Inject constructor(
@@ -18,7 +21,11 @@ class NewsRemoteRepositoryImpl @Inject constructor(
         pagingSourceFactory = {
             NewsRemotePagingSource(newsService = newsService, category = category)
         }
-    ).flow.catch { e ->  
+    ).flow.map { pagingData ->
+        pagingData.map {
+            RemoteMapper.mapToNewsItem(it)
+        }
+    }.catch { e ->
         Log.e("AppTest", "error : ${e}")
     }
 
