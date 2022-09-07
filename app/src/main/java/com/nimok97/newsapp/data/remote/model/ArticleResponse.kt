5cf9b7698bbdc73438c04a57cecd73d8ebcbd9b4
한창희 <nimok97@gmail.com>
2022-09-07 22:04:57 +0900

    [FIX] #5 리모트 데이터 null 대응

--- app/src/main/java/com/nimok97/newsapp/data/remote/model/ArticleResponse.kt
+++ app/src/main/java/com/nimok97/newsapp/data/remote/model/ArticleResponse.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ArticleResponse(
     @SerialName("author")
-    val author: String,
+    val author: String = "no author",
     @SerialName("title")
     val title: String,
     @SerialName("description")
